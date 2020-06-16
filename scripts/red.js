2822217e7983f3b9cd49f2d780a351548449b73a
JH Kwon <dev@obin.kr>
2020-06-17 04:44:34 +0900

    change redirect destination

--- scripts/red.js
+++ scripts/red.js
@@ -1,7 +1,2 @@
-var sear = window.location.search.substr(1);
-var dp = $.deparam(sear);
-var x = Object.keys(dp)[0]; // drs.obin.kr/?username
-var par = (x == undefined? "https://dancerush-info.herokuapp.com": "https://dancerush-info.herokuapp.com/user/" + x);
-// document.getElementById("uname").textContent = "If not redirected, click <a href=\"" + par + "\">here</a>.";
-document.write("If not redirected, click <a href=\"" + par + "\">here</a>.");
+document.write("If not redirected, click <a href=\"https://drs.obin.kr\">here</a>.");
 window.location.href = par;
